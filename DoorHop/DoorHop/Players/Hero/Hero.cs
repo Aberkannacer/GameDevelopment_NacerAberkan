65a3ea213e9477a6ba47fe811a37d89afd2b0fdb
Nacer <aberkannacer04@gmail.com>
2024-12-05 21:42:12 +0100

    Making hero class

--- DoorHop/DoorHop/Players/Hero/Hero.cs
+++ DoorHop/DoorHop/Players/Hero/Hero.cs
@@ -1,8 +1,12 @@
-﻿using DoorHop.Input;
+﻿using DoorHop.Animation;
+using DoorHop.Input;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,14 +14,55 @@ namespace DoorHop.Players.Hero
 {
     public class Hero : Player
     {
+        private Animatie animatie;
         public Hero(ContentManager content, IInputReader inputReader) : base(content, inputReader)
         {
 
         }
 
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
+
+        public override void Update(GameTime gameTime, List<TileMap.CollisionTiles> tiles)
+        {
+            base.Update(gameTime, tiles);
+        }
+
+        protected override void CheckXCollision(List<TileMap.CollisionTiles> tiles)
+        {
+            base.CheckXCollision(tiles);
+        }
+
+        protected override void CheckYCollision(List<TileMap.CollisionTiles> tiles)
+        {
+            base.CheckYCollision(tiles);
+        }
+
+        protected override void HandleInput()
+        {
+            base.HandleInput();
+        }
+
         protected override void LoadContent(ContentManager contentManager)
         {
-            throw new NotImplementedException();
+            animatie = new Animatie(contentManager.Load<Texture2D>("Player"), true);
+        }
+
+        protected override void SetAnimationSpeed(float speed)
+        {
+            base.SetAnimationSpeed(speed);
+        }
+
+        protected override void SetJumpForce(float force)
+        {
+            base.SetJumpForce(force);
+        }
+
+        protected override void SetMoveSpeed(float speed)
+        {
+            base.SetMoveSpeed(speed);
         }
     }
 
--- DoorHop/DoorHop/obj/Debug/net6.0-windows/DoorHop.AssemblyInfo.cs
+++ DoorHop/DoorHop/obj/Debug/net6.0-windows/DoorHop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoorHop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dd973aaa1f27f9bf063fb0f12f7203fbaa0c349")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61d871ec60239baabdf6ab5a2f2be891ef0035d9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoorHop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoorHop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
