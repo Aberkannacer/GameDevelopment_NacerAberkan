5501d51e42e0a0cd4de856dfaa637e25b50a4658
Nacer <aberkannacer04@gmail.com>
2024-12-25 05:18:57 +0100

    adding level class

--- DoorHop/DoorHop/obj/Debug/net6.0-windows/DoorHop.AssemblyInfo.cs
+++ DoorHop/DoorHop/obj/Debug/net6.0-windows/DoorHop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoorHop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52404b98bb772ca99ea272b1a11e48894859662f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a239a8b877c3369713d6109eddfce7cdfb129a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoorHop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoorHop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
