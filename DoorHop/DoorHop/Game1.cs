fb99066439ef9df0688451bd0fcb577d9ef18775
Nacer <aberkannacer04@gmail.com>
2024-12-29 23:11:05 +0100

    cleaned everything + game class

--- DoorHop/DoorHop/Game1.cs
+++ DoorHop/DoorHop/Game1.cs
@@ -1,70 +1,45 @@
-﻿using DoorHop.Players;
-using DoorHop.Players.Enemys;
-using DoorHop.Players.Heros;
-using DoorHop.TileMap;
-using Microsoft.Xna.Framework;
+﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using System.Collections.Generic;
-using System;
-using System.Linq;
-using DoorHop.Input;
-using System.Diagnostics;
-using DoorHop.Collectables;
 using DoorHop.GameStates;
-using Microsoft.Xna.Framework.Media;
 
 namespace DoorHop
 {
     public class Game1 : Game
     {
+        //grafics
         private GraphicsDeviceManager graphics;
         private SpriteBatch _spriteBatch;
-    
+        //textures
         private Texture2D backgroundTexture;
         private Rectangle backgroundRect;
-
-
+        //states
         private State currentState;
         private State nextState;
-
-        
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
-            graphics.PreferredBackBufferWidth = 800; // Stel de breedte in
+            //schermbreedte en hoogt
+            graphics.PreferredBackBufferWidth = 800;
             graphics.PreferredBackBufferHeight = 480;
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
-
         }
-        
-
         protected override void Initialize()
         {
             base.Initialize();
             ChangeState(new MenuState(this, Content, graphics.GraphicsDevice));
-
-
         }
 
         protected override void LoadContent()
         {
+            //sprite
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            //state
             currentState = new MenuState(this, Content, GraphicsDevice);
             currentState.LoadContent();
-
-
-            /*Tiles.Content = Content;*/
-
+            //texture
             backgroundTexture = Content.Load<Texture2D>("background");
-
-            
-            
-
-
         }
         protected override void Update(GameTime gameTime)
         {
@@ -73,42 +48,25 @@ namespace DoorHop
                 {
                     currentState = nextState;
                     currentState.LoadContent();
-
                     nextState = null;
                 }
-
                 currentState.Update(gameTime);
-
             }
-
             base.Update(gameTime);
         }
-
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-
             _spriteBatch.Begin();
-
             currentState.Draw(gameTime, _spriteBatch);
-
-            // Teken eerst de achtergrond
             _spriteBatch.Draw(backgroundTexture, backgroundRect, Color.White);
-
-            
-
-
             _spriteBatch.End();
-
             base.Draw(gameTime);
         }
-
-        
         public void ChangeState(State state)
         {
             nextState = state;
         }
-        
     }
 }
--- DoorHop/DoorHop/obj/Debug/net6.0-windows/DoorHop.AssemblyInfo.cs
+++ DoorHop/DoorHop/obj/Debug/net6.0-windows/DoorHop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DoorHop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5072da30425c9995ab2394396ab4d8fc42506275")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c6eead0f9bbdeaab61f7e608f92d3b8bda66525")]
 [assembly: System.Reflection.AssemblyProductAttribute("DoorHop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DoorHop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
